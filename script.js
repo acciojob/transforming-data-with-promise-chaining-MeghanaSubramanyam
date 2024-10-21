741d774ff636ad5f39b42fba699f75672b1c05c7
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-10-21 11:39:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+let btn=document.getElementById("btn");
+let input=document.getElementById("ip");
+let output=docmunet.getElementById("output");
+btn.addEventListener("click",function pro(){
+	const inputValue=input.value;
+	console.log(inputValue);
+	})
+
+let promise=new Promise((res,rej)=>{
+	setTimeout(()=>{
+		res(inputValue)
+	},2000)
+})
+promise.then((result)=>{
+	return new Promise((res,rej)=>{
+		setTimeout(()=>{
+			res(result*2);
+		},1000)
+	})
+})
+
+promise.then((result)=>{
+	return new Promise((res,rej)=>{
+		setTimeout(()=>{
+			res(result-3);
+		},1000)
+	})
+})
+promise.then((result)=>{
+	return new Promise((res,rej)=>{
+		setTimeout(()=>{
+			res(result/2);
+		},1000)
+	})
+})
+promise.then((result)=>{
+	return new Promise((res,rej)=>{
+		setTimeout(()=>{
+			res(result+10);
+		},1000)
+	})
+})
+.then((result)=>{
+	output.textContent=`Result:${result}`
+})
+.catch((e)=>{
+	console.log(e)
+})
